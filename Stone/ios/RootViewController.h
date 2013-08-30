28ab4fca2da40d5ee678c8868c1921481a3b1dd5
Lllong <lxl3721@qq.com>
2013-08-30 11:00:18 +0800

    Initial Commit

---
+++ Stone/ios/RootViewController.h
@@ -0,0 +1,16 @@
+//
+//  StoneAppController.h
+//  Stone
+//
+//  Created by Li Zhixuan on 8/30/13.
+//  Copyright __MyCompanyName__ 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+
+@interface RootViewController : UIViewController {
+
+}
+
+@end
